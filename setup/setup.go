24b0e8f78d19d43474c0013e1c5d8124bcb6a511
notTGY <microcraft47@gmail.com>
2025-04-27 11:16:54 +0300

    remove colors

---
+++ setup/setup.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	spinner   spinner.Model
+	choice    int
+	confirmed bool
+	quitting  bool
+}
+
+func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	return model{
+		spinner:   s,
+		choice:    0,
+		confirmed: false,
+		quitting:  false,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.spinner.Tick
+}
+
+type finishMsg struct {}
+
+func installDocker() tea.Cmd {
+  return func() tea.Msg {
+    // actually install docker
+    time.Sleep(1 * time.Second)
+
+    return finishMsg{}
+  }
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+      case "enter", "y", "Y":
+        m.confirmed = true
+        return m, installDocker()
+      case "n", "esc", "ctrl+c":
+        m.quitting = true
+        return m, tea.Quit
+		}
+  case finishMsg:
+    return m, tea.Quit
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.confirmed {
+		return fmt.Sprintf(
+			"%s Installing docker...\n",
+			m.spinner.View(),
+		)
+	}
+	if m.quitting {
+		return "Cancelled.\n"
+	}
+	return fmt.Sprintf("Install docker? [Y/n]")
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+  fmt.Printf("Installing required packages\n")
+  time.Sleep(3 * time.Second)
+
+
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
